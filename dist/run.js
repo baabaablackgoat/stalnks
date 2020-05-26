e3614fbac0312222b0193ae464cf3d876b3535ac
FelixRilling <contact@rilling.dev>
2020-05-26 18:50:49 +0200

    Extracted UserEntry to file.

--- dist/run.js
+++ dist/run.js
@@ -1,18 +1,4 @@
 "use strict";
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to set private field on non-instance");
-    }
-    privateMap.set(receiver, value);
-    return value;
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
-    if (!privateMap.has(receiver)) {
-        throw new TypeError("attempted to get private field on non-instance");
-    }
-    return privateMap.get(receiver);
-};
-var __weekPrices;
 Object.defineProperty(exports, "__esModule", { value: true });
 const Discord = require("discord.js");
 const discord_js_1 = require("discord.js");
@@ -21,6 +7,7 @@ const fs = require("fs");
 const Queue_1 = require("./classes/Queue");
 const getEnv_1 = require("./functions/getEnv");
 const Client_1 = require("./functions/Client");
+const UserEntry_1 = require("./classes/UserEntry");
 // ensure data exists
 if (!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
@@ -62,7 +49,7 @@ fs.readFile(userDataPath, 'utf8', (err, data) => {
             const rawData = JSON.parse(data);
             for (const key in rawData) {
                 try {
-                    userData[key] = UserEntry.fromRaw(key, rawData[key]);
+                    userData[key] = UserEntry_1.UserEntry.fromRaw(key, rawData[key]);
                 }
                 catch (entryErr) {
                     console.log("Non-fatal error raised while reading userData from JSON: " + entryErr);
@@ -171,82 +158,6 @@ function hasElevatedPermissions(member) {
     }
     return false;
 }
-class UserEntry {
-    constructor(id, timezone, friendcode, weekUpdated, lastWeekPattern, _weekPrices, optInPatternDM) {
-        __weekPrices.set(this, void 0);
-        this.id = id;
-        this.timezone = timezone;
-        this.friendcode = friendcode;
-        this.weekUpdated = weekUpdated;
-        this.lastWeekPattern = lastWeekPattern; // Fluctuating: 0, Large Spike: 1, Decreasing: 2, Small Spike: 3, I don't know: any
-        __classPrivateFieldSet(// Fluctuating: 0, Large Spike: 1, Decreasing: 2, Small Spike: 3, I don't know: any
-        this, __weekPrices, _weekPrices);
-        this.optInPatternDM = optInPatternDM;
-    }
-    get weekPrices() {
-        const currWeek = moment().tz(this.timezone).week();
-        if (this.weekUpdated != currWeek) {
-            console.log(`Cleared week price data for ${this.id}.`);
-            this.lastWeekPattern = undefined;
-            if (this.optInPatternDM) { // Asking for the previous pattern
-                Client_1.default.users.fetch(this.id)
-                    .then(user => {
-                    const patternEmoji = {
-                        largeSpike: "💸",
-                        smallSpike: "📈",
-                        fluctuating: "📊",
-                        decreasing: "📉",
-                    };
-                    const patternNumbers = {
-                        fluctuating: 0,
-                        largeSpike: 1,
-                        decreasing: 2,
-                        smallSpike: 3
-                    };
-                    const askForLastPatternEmbed = new Discord.MessageEmbed({
-                        description: `It seems like you've entered turnip prices last week that have now run their course!\nDo you know which pattern your turnip prices were following **last week?**\nPlease use the reactions below to enter your pattern. If you don't know your pattern, you can ignore this message.\n\n${patternEmoji.largeSpike} Large spike \n${patternEmoji.smallSpike} Small spike \n${patternEmoji.fluctuating} Fluctuating \n${patternEmoji.decreasing} Decreasing`,
-                        color: "LUMINOUS_VIVID_PINK",
-                    });
-                    user.send(askForLastPatternEmbed)
-                        .then(sentMessage => {
-                        for (const key in patternEmoji) {
-                            sentMessage.react(patternEmoji[key]).catch(err => console.error(err));
-                        }
-                        const patternCollector = sentMessage.createReactionCollector((r, u) => !u.bot && Object.values(patternEmoji).includes(r.emoji.name), {
-                            time: 5 * 60 * 1000,
-                            max: 1
-                        });
-                        patternCollector.on("end", (collected, reason) => {
-                            if (reason == 'time' || collected.size < 1)
-                                return;
-                            this.lastWeekPattern = patternNumbers[Object.keys(patternEmoji).find(key => patternEmoji[key] == collected.first().emoji.name)];
-                        });
-                    })
-                        .catch(err => console.log("Failed to message user to ask about last week's pattern: " + err));
-                })
-                    .catch(err => console.log("Failed to lookup user to ask about last week's pattern: " + err));
-            }
-            __classPrivateFieldSet(this, __weekPrices, Array(13).fill(''));
-        }
-        this.weekUpdated = currWeek;
-        return __classPrivateFieldGet(this, __weekPrices);
-    }
-    get filledWeekPrices() {
-        const lastFilledIndex = this.weekPrices.map((k) => Boolean(k)).lastIndexOf(true) + 1;
-        return this.weekPrices.slice(0, lastFilledIndex);
-    }
-    get turnipProphetURL() {
-        const pricesString = this.filledWeekPrices.join('.');
-        if (pricesString.length > 0)
-            return `https://turnipprophet.io?prices=${pricesString}${this.lastWeekPattern !== undefined ? "&pattern=" + this.lastWeekPattern : ""}`;
-        else
-            return `https://turnipprophet.io${this.lastWeekPattern !== undefined ? "?pattern=" + this.lastWeekPattern : ""}`;
-    }
-    static fromRaw(id, obj) {
-        return new UserEntry(id, obj.timezone, obj.friendcode, obj.hasOwnProperty('weekUpdated') ? obj.weekUpdated : moment().tz(obj.timezone).week(), obj.hasOwnProperty('lastWeekPattern') ? obj.lastWeekPattern : undefined, obj.hasOwnProperty('_weekPrices') ? obj._weekPrices : Array(13).fill(''), obj.hasOwnProperty('optInPatternDM') ? obj.optInPatternDM : true);
-    }
-}
-__weekPrices = new WeakMap();
 class PriceEntry {
     constructor(userId, price, expiresAt = null) {
         // sanity checks. these *can* be made redundant, but you can also just handle errors
@@ -605,7 +516,7 @@ Client_1.default.on('message', msg => {
         if (userData.hasOwnProperty(msg.author.id))
             userData[msg.author.id].timezone = timezone;
         else
-            userData[msg.author.id] = new UserEntry(msg.author.id, timezone, null, null, null, null, true);
+            userData[msg.author.id] = new UserEntry_1.UserEntry(msg.author.id, timezone, null, null, null, null, true);
         if (inaccurateTimezones.includes(timezone)) {
             const confirmDangerousTimezoneSetEmbed = new Discord.MessageEmbed({
                 author: { name: msg.member.displayName, iconURL: msg.author.avatarURL() },
@@ -667,7 +578,7 @@ Client_1.default.on('message', msg => {
             return;
         }
         else {
-            userData[msg.author.id] = new UserEntry(msg.author.id, null, fc, null, null, null, true);
+            userData[msg.author.id] = new UserEntry_1.UserEntry(msg.author.id, null, fc, null, null, null, true);
             const profileWithFriendcodeCreatedEmbed = new Discord.MessageEmbed({
                 author: { name: msg.member.displayName, iconURL: msg.author.avatarURL() },
                 color: 4289797,
--- run.ts
+++ run.ts
@@ -1,10 +1,11 @@
 import * as Discord from "discord.js";
-import {TextChannel} from "discord.js";
+import { TextChannel } from "discord.js";
 import * as moment from "moment-timezone";
 import * as fs from "fs";
-import {QueueEntry} from "./classes/Queue";
+import { QueueEntry } from "./classes/Queue";
 import getEnv from "./functions/getEnv";
 import client from "./functions/Client";
+import { UserEntry } from "./classes/UserEntry";
 
 // ensure data exists
 if (!fs.existsSync('./data')){
@@ -163,98 +164,6 @@ function hasElevatedPermissions(member): boolean {
 		}
 	}
 	return false;
-}
-
-
-class UserEntry {
-	readonly id: string;
-	timezone: string | null;
-	friendcode: string | null;
-	weekUpdated: number | null;
-	lastWeekPattern: number | null;
-	#_weekPrices: Array<string | number> | null;
-	optInPatternDM: boolean;
-
-	constructor(id: string, timezone: string | null, friendcode: string | null, weekUpdated: number | null, lastWeekPattern: number | null, _weekPrices: Array<string | number> | null, optInPatternDM: boolean) {
-		this.id = id;
-		this.timezone = timezone;
-		this.friendcode = friendcode;
-		this.weekUpdated = weekUpdated;
-		this.lastWeekPattern = lastWeekPattern; // Fluctuating: 0, Large Spike: 1, Decreasing: 2, Small Spike: 3, I don't know: any
-		this.#_weekPrices = _weekPrices;
-		this.optInPatternDM = optInPatternDM;
-	}
-
-	get weekPrices(): Array<string | number> | null {
-		const currWeek = moment().tz(this.timezone).week();
-		if (this.weekUpdated != currWeek) {
-			console.log(`Cleared week price data for ${this.id}.`);
-			this.lastWeekPattern = undefined;
-			if (this.optInPatternDM) { // Asking for the previous pattern
-				client.users.fetch(this.id)
-					.then(user => {
-						const patternEmoji = {
-							largeSpike: "💸",
-							smallSpike: "📈",
-							fluctuating: "📊",
-							decreasing: "📉",
-						};
-						const patternNumbers = {
-							fluctuating: 0,
-							largeSpike: 1,
-							decreasing: 2,
-							smallSpike: 3
-						};
-						const askForLastPatternEmbed = new Discord.MessageEmbed({
-							description: `It seems like you've entered turnip prices last week that have now run their course!\nDo you know which pattern your turnip prices were following **last week?**\nPlease use the reactions below to enter your pattern. If you don't know your pattern, you can ignore this message.\n\n${patternEmoji.largeSpike} Large spike \n${patternEmoji.smallSpike} Small spike \n${patternEmoji.fluctuating} Fluctuating \n${patternEmoji.decreasing} Decreasing`,
-							color: "LUMINOUS_VIVID_PINK",
-						});
-						user.send(askForLastPatternEmbed)
-							.then(sentMessage => {
-								for (const key in patternEmoji) {
-									sentMessage.react(patternEmoji[key]).catch(err => console.error(err));
-								}
-								const patternCollector = sentMessage.createReactionCollector((r, u) => !u.bot && Object.values(patternEmoji).includes(r.emoji.name), {
-									time: 5 * 60 * 1000,
-									max: 1
-								});
-								patternCollector.on("end", (collected, reason) => {
-									if (reason == 'time' || collected.size < 1) return;
-									this.lastWeekPattern = patternNumbers[Object.keys(patternEmoji).find(key => patternEmoji[key] == collected.first().emoji.name)];
-								});
-							})
-							.catch(err => console.log("Failed to message user to ask about last week's pattern: " + err));
-					})
-					.catch(err => console.log("Failed to lookup user to ask about last week's pattern: " + err));
-			}
-			this.#_weekPrices = Array(13).fill('');
-		}
-		this.weekUpdated = currWeek;
-		return this.#_weekPrices;
-	}
-
-	get filledWeekPrices(): Array<string | number> {
-		const lastFilledIndex = this.weekPrices.map((k) => Boolean(k)).lastIndexOf(true) + 1;
-		return this.weekPrices.slice(0, lastFilledIndex);
-	}
-
-	get turnipProphetURL(): string {
-		const pricesString = this.filledWeekPrices.join('.');
-		if (pricesString.length > 0) return `https://turnipprophet.io?prices=${pricesString}${this.lastWeekPattern !== undefined ? "&pattern=" + this.lastWeekPattern : ""}`;
-		else return `https://turnipprophet.io${this.lastWeekPattern !== undefined ? "?pattern=" + this.lastWeekPattern : ""}`;
-	}
-
-	static fromRaw(id, obj): UserEntry {
-		return new UserEntry(
-			id,
-			obj.timezone,
-			obj.friendcode,
-			obj.hasOwnProperty('weekUpdated') ? obj.weekUpdated : moment().tz(obj.timezone).week(),
-			obj.hasOwnProperty('lastWeekPattern') ? obj.lastWeekPattern : undefined,
-			obj.hasOwnProperty('_weekPrices') ? obj._weekPrices : Array(13).fill(''),
-			obj.hasOwnProperty('optInPatternDM') ? obj.optInPatternDM : true
-		);
-	}
 }
 
 
